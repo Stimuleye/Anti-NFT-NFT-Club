e9b389bb3d04f664a8bf5aa101150740fb813347
Stimuleye <99770764+Stimuleye@users.noreply.github.com>
2022-02-21 23:24:15 -0800

    Update App.js

--- nft-minting-app-main/src/App.js
+++ nft-minting-app-main/src/App.js
@@ -77,11 +77,11 @@ export const StyledLogo = styled.img`
 export const StyledImg = styled.img`
   box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.7);
   border: 0px dashed var(--secondary);
-  background-color: none;
+  background-color: black;
   border-radius: 100%;
-  width: 200px;
+  width: 350px;
   @media (min-width: 900px) {
-    width: 250px;
+    width: 350px;
   }
   @media (min-width: 1000px) {
     width: 300px;
